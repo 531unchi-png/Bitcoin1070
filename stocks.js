8c877549862906a3cbdc8200aae01561410fa898
531unchi-png <531unchi@gmail.com>
2026-07-11 20:01:47 +0900

    Update stocks.js

--- stocks.js
+++ stocks.js
@@ -1,1 +1,48 @@
+// ===============================
+// Stock Manager
+// ===============================
 
+// 仮データ（あとでAPI取得に変更）
+const stockPrices = {
+
+    NVDA: 185,      // USD
+    USDJPY: 160,
+
+    MHI: 3650,
+    ADVT: 11800,
+    FJK: 7200,
+    VRAIN: 3950
+
+};
+
+// 円換算評価額
+function calculateStockAssets(){
+
+    const stock = {
+
+        NVDA:
+            holdings.NVDA *
+            stockPrices.NVDA *
+            stockPrices.USDJPY,
+
+        MHI:
+            holdings.MHI *
+            stockPrices.MHI,
+
+        ADVT:
+            holdings.ADVT *
+            stockPrices.ADVT,
+
+        FJK:
+            holdings.FJK *
+            stockPrices.FJK,
+
+        VRAIN:
+            holdings.VRAIN *
+            stockPrices.VRAIN
+
+    };
+
+    return stock;
+
+}
