169b8c11abb8193e8b24762714010c3c5a9853b9
531unchi-png <531unchi@gmail.com>
2026-07-11 09:05:54 +0900

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,1 +1,30 @@
+// ===============================
+// Chart Manager
+// ===============================
 
+function drawPortfolioChart(){
+
+    const crypto =
+        holdings.BTC +
+        holdings.ETH +
+        holdings.XRP +
+        holdings.SOL +
+        holdings.SUI +
+        holdings.RENDER;
+
+    const japan =
+        holdings.MHI +
+        holdings.ADVT +
+        holdings.FJK +
+        holdings.VRAIN;
+
+    const usa =
+        holdings.NVDA;
+
+    console.log("Crypto", crypto);
+    console.log("Japan", japan);
+    console.log("USA", usa);
+
+}
+
+drawPortfolioChart();
